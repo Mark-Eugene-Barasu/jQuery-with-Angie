028fb9477948369bae633ae5074a5dac76681069
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-08-29 06:20:59 +0200

    better way of calling an event listener method in jQuery

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ $(document).keypress(function(event) {
     // display the key output in h1 element
     $(`h1`).text(event.key);
 })
+
+// a in improved way to add event
+let h1 = $(`h1`); 
+h1.on(`mouseover`,  function(){
+    h1.css(`color`, `purple`);
+})
+
+// beautiful, check mozilla to check for more event listener
