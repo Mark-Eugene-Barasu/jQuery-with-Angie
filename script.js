7ad3b827ffbd410cd928c7d66971f1b4640ee57c
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-08-29 04:32:50 +0200

    editing the style using jQuery in JavaScript.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+// this is how to set style in JavaScript using jQuery.
+// but it not best practice to keep CSS in JS
+// to in the next commit well create the style in CSS and using it here using jQuery
 $(`h1`).css(`color`, `red`);
+
+$(`button`).css(`backgroundImage`, `linear-gradient(160deg,white, lightblue, chartreuse)`);
