bd6463dca3070649ba1b08ff9f8b770f2ddf007a
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-08-29 05:40:27 +0200

    manipulating and displaying classes applied

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ $(`a`).attr(`href`, `https://mozilla.com`);
 console.log($(`a`).attr(`href`));
 
 
-// since class is also an attribute
-console.log($(`h1`).attr(`class`))
 
+// since class is also an attribute
+$(`h1`).addClass(`fountain`); // applying class in jQuery
+
+// the class="another" from html
+console.log($(`h1`).attr(`class`)) // display all classes applied
