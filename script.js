fc48fdbe11957609d560d5cacb91947ab5c4f605
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-08-30 05:10:05 +0200

    YOU DON'T HAVE TO MEMORIZE ANY CODE, CODING IS OPEN BOOK EXAMS

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// short assignment
-
-$(document).keypress(function(event) {
-    console.log(event.key);
-    // display the key output in h1 element
-    $(`h1`).text(event.key);
-})
 
 // a in improved way to add event
 let h1 = $(`h1`); 
@@ -12,4 +5,5 @@ h1.on(`mouseover`,  function(){
     h1.css(`color`, `purple`);
 })
 
-// beautiful, check mozilla to check for more event listener
+// YOU NEVER HAVE TO MEMORIZE ANY OF THE CODE,
+// JUST KNOW THAT THEY EXIST AND WHAT THEY DO WHEN APPLIED
