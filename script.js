a48b6ff439232c16e85c80d87564f9c120715f39
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-09-03 20:51:03 +0200

    revision on keypress eventListener event

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ h1.on(`mouseover`,  function(){
 
 // YOU NEVER HAVE TO MEMORIZE ANY OF THE CODE,
 // JUST KNOW THAT THEY EXIST AND WHAT THEY DO WHEN APPLIED
+
+// short assignment
+
+$(document).keypress(function(event) {
+    console.log(event.key);
+    // display the key output in h1 element
+    $(`h1`).text(event.key);
+});
