442ed57a700d0b00adb417a2806b148121b4151a
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-09-03 21:23:18 +0200

    this is an alternative way to make jQuery work but it not a best practice.

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-// jQuery creates properly groups each and every element
-// into its own DOM object
 
-if ($(`h1`) == document.querySelector(`h1`)){
-    console.log(`same`);
-} else {
-    console.log(`not same`);
-}
-
-console.log($(`h1`));
-console.log(document.querySelector(`h1`))
+$(document).ready(function () {
+    
+    $(`h1`).css(`color`, `red`);
+})
