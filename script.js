46c935c32e67855413355bb47f1294f5dc16488c
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-09-08 17:19:09 +0200

    / using .before, .after, .prepend, and .append

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 
-// here is the best practice instead
+// using .before, .after, .prepend, and .append
 
 $(`h1`).attr(`class`, `another fountain`)
 
-$(`input`).on(`keypress`, function(event) {
-    console.log(event.key);
-    $(`h1`).text(event.key);
-});
+// using before adds before the element
+$(`h1`).before(`<button>new</button>`)
 
+// using after adds after the element
+$(`h1`).after(`<button>New</button>`)
+
+
+// now, using prepend add before the element's content
+$(`h1`).prepend(`<button>prepend</button>`)
+
+// also, append adds to the end of the element's content
+$(`h1`).append(`<button>append</button>`)
