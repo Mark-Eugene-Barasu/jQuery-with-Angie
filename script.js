842ac0a3a085d0dc9592ddf1e8a5c7e767730210
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-09-08 18:17:10 +0200

    using .hide, .show, .toggle, .fadeOut, .fadeIn, .fadeToggle, .slideDown, .slideUp, .slideTogglem, and .animate

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-// see last line
-// using .before, .after, .prepend, and .append
+
+// using .hide, .show, .toggle, .fadeOut, .fadeIn, .fadeToggle,
+// .slideDown, .slideUp, .slideToggle
+// .animate help to customize your own animation CSS
 
 $(`h1`).attr(`class`, `another fountain`)
 
-// using before adds before the element
-$(`h1`).before(`<button>new</button>`)
+$(`button`).on(`click`, function() {
+    // everything will happen in that order
+    $(`h1`).slideUp().slideDown().animate({opacity: `0.7`,})
+});
 
-// using after adds after the element
-$(`h1`).after(`<button>New</button>`)
-
-
-// now, using prepend add before the element's content
-$(`h1`).prepend(`<button>prepend</button>`)
-
-// also, append adds to the end of the element's content
-$(`h1`).append(`<button>append</button>`)
-
-// using .remote to remote every jQuery mutation
-$(`h1`).remove();
