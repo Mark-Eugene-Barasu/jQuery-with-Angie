6ea79be5bdfb4d0e3fed2afa6f31b31986e60919
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-09-08 17:06:00 +0200

    ALWAYS CHECK MOZILLA.ORG FOR EVENT SPELLING BEFORE USING THEM.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 // here is the best practice instead
 
-$(`h1`).css(`color`, `purple`);
-$(`h1`).attr(`class`, `fountain another`)
-// $(`h1`).removeClass(`fountain`)
-$(`h1`).attr(`class`, `fountain`)
-console.log($(`h1`).hasClass(`fountain`));
-console.log($(`h1`).hasClass(`another`));
-console.log($(`h1`).attr(`class`));
+$(`h1`).attr(`class`, `another fountain`)
+
+$(`input`).on(`keypress`, function(event) {
+    console.log(event.key);
+    $(`h1`).text(event.key);
+});
+
