ccc169ea282ce2f8dc9f1e005e687547c359d17c
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-09-08 16:54:53 +0200

    since class is also an attribute, you could also manipulate it as an attribute with two parameters with the first para being a , and second being

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
 // here is the best practice instead
 
-$(`h1`).css(`color`, `red`);
+$(`h1`).css(`color`, `purple`);
+$(`h1`).attr(`class`, `fountain another`)
+// $(`h1`).removeClass(`fountain`)
+$(`h1`).attr(`class`, `fountain`)
+console.log($(`h1`).hasClass(`fountain`));
+console.log($(`h1`).hasClass(`another`));
+console.log($(`h1`).attr(`class`));
